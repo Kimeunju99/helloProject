85b070bfb537a8a8c0b0b510199a153cd5d99294
admin <admin@DESKTOP-H97OAL1>
2023-05-30 10:47:44 +0900

    new commit

--- HelloApp/src/com/yedam/HelloWorld.java
+++ HelloApp/src/com/yedam/HelloWorld.java
@@ -1,8 +1,8 @@
 package com.yedam;
 
 public class HelloWorld {
-	public static void main(String[] args){
-		System.out.println("Hello World");
-		System.out.println("git 연동");
-	}
+		public static void main(String[] args){
+			System.out.println("Hello World");
+			System.out.println("git 연동");
+		}	
 }
