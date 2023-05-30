bebbdb045fe1451c02793ec40ed65a05df4a1347
admin <admin@DESKTOP-H97OAL1>
2023-05-30 10:41:44 +0900

    first commit

---
+++ HelloApp/src/com/yedam/HelloApp.java
@@ -0,0 +1,12 @@
+package com.yedam;
+
+public class HelloApp {
+	public static void main(String[] args) {
+		int num1 = 100;
+		int num2 = 50;
+		int num3 = 10; //add code
+		int result = num1 + num2;
+		result = result - num3; //add code
+		System.out.println("결과값: " + result);
+	}//end of main
+}//end of class
